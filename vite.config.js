d8d282df784d9aaff9cb8ed599ad05ab6e0f063a
Cristian Rodriguez Benitez <126631418+CritianRodriguez042502@users.noreply.github.com>
2023-11-09 09:54:49 -0500

    Cambiando el base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/agency_view/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000,
