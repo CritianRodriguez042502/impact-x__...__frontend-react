4aae209345c95bd8f914ccdff9f38de7ec4a709e
Cristian Rodriguez Benitez <126631418+CritianRodriguez042502@users.noreply.github.com>
2023-11-22 15:23:28 -0500

    Test

--- src/components/common/likes/LikesBlog.jsx
+++ src/components/common/likes/LikesBlog.jsx
@@ -10,7 +10,6 @@ import { BiSolidLike } from "react-icons/bi";
 //---------------------------------------------------
 import style from "./style_likes.module.css";
 
-
 export function LikesBlog({ params }) {
   const dispatch = useDispatch();
 
@@ -18,7 +17,10 @@ export function LikesBlog({ params }) {
   const infoGetLikeBlog = useSelector((state) => state.getLikesBlog);
   const Infolike = useSelector((state) => state.likeBlog);
 
-  const [selectLikeUser, setSelectLikeUser] = useState(<BiLike />);
+  const [selectLikeUser, setSelectLikeUser] = useState({
+    icon: <BiLike />,
+    selected: false,
+  });
 
   // Style loader like
   const [overlay, setOverlay] = useState("none");
@@ -40,13 +42,22 @@ export function LikesBlog({ params }) {
           return data.user.username === username;
         }
       );
-      if (like_registered.length == 0) {
-        setSelectLikeUser(<BiLike />);
+      if (!like_registered.length) {
+        setSelectLikeUser({
+          icon: <BiLike />,
+          selected: false,
+        });
       } else {
-        if (like_registered[0].selected == true) {
-          setSelectLikeUser(<BiSolidLike />);
+        if (like_registered[0].selected === true) {
+          setSelectLikeUser({
+            icon: <BiSolidLike />,
+            selected: true,
+          });
         } else {
-          setSelectLikeUser(<BiLike />);
+          setSelectLikeUser({
+            icon: <BiLike />,
+            selected: false,
+          });
         }
       }
     }
@@ -59,42 +70,44 @@ export function LikesBlog({ params }) {
     }, 3000);
   }, [overlay]);
 
-  function onChangeLike(e) {
-    if (
-      selectLikeUser.type.name === "BiLike" &&
-      infoJWTVerify.status === "fulfilled" &&
-      access &&
-      username
-    ) {
-      console.log(selectLikeUser.type.name)
-      const updatedLike = {
-        like: true,
-        slug: params,
-        jwt: access,
-      };
-      import("../../../redux/index").then((modules) => {
-        dispatch(modules.axiosLikeBlog(updatedLike));
-        setOverlay("inline-block");
-        setVisibilityLike("none");
-      });
-      setSelectLikeUser(<BiSolidLike />);
-    } else if (
-      selectLikeUser.type.name === "BiSolidLike" &&
-      infoJWTVerify.status === "fulfilled" &&
-      access &&
-      username
-    ) {
-      const updatedLike = {
-        like: false,
-        slug: params,
-        jwt: access,
-      };
-      import("../../../redux/index").then((modules) => {
-        setOverlay("inline-block");
-        setVisibilityLike("none");
-        dispatch(modules.axiosLikeBlog(updatedLike));
-      });
-      setSelectLikeUser(<BiLike />);
+  // Function like of dislike
+  function onChangeLike() {
+    // if user have register
+    if (infoJWTVerify.status === "fulfilled" && access && username) {
+      if (!selectLikeUser.selected) {
+        import("../../../redux/index").then((modules) => {
+          dispatch(
+            modules.axiosLikeBlog({
+              like: true,
+              slug: params,
+              jwt: access,
+            })
+          );
+          setOverlay("inline-block");
+          setVisibilityLike("none");
+          setSelectLikeUser({
+            icon: <BiSolidLike />,
+            selected: true,
+          });
+        });
+      } else {
+        import("../../../redux/index").then((modules) => {
+          setOverlay("inline-block");
+          setVisibilityLike("none");
+          dispatch(
+            modules.axiosLikeBlog({
+              like: false,
+              slug: params,
+              jwt: access,
+            })
+          );
+        });
+        setSelectLikeUser({
+          icon: <BiLike />,
+          selected: false,
+        });
+      }
+    // If not
     } else {
       Swal.fire({
         icon: "warning",
@@ -116,7 +129,7 @@ export function LikesBlog({ params }) {
               className={style.mainButton}
               onClick={onChangeLike}
             >
-              {selectLikeUser}
+              {selectLikeUser.icon}
             </span>
             <span className={style.counter}>
               {infoGetLikeBlog.info.data.all_likes}
