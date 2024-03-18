4f7a2302cf68185db97609f1ca62e34fc6a5ff5f
Cristian <cuentagit1111@gmail.com>
2024-03-18 08:56:18 -0500

    Last commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000,
+    chunkSizeWarningLimit: 3500,
   },
 });
